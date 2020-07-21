2ea24a242745577c4494134cecb9607b2a40698a
Akhil Durvasula <akhil.durvasula06@gmail.com>
2020-07-21 13:03:27 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by sastry d on 7/21/20.
+//  Copyright © 2020 Akhil Durvasula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
